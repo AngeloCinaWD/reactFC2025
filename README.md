6d2ba91433ea16e75efa5e614cc5fbbe41585e31
unknown <a.cina@procreatech.com>
2025-02-08 21:37:32 +0100

    final commit useEffect

--- README.md
+++ README.md
@@ -9,3 +9,11 @@
 [className](https://stackoverflow.com/questions/55445194/what-does-the-classname-attribute-mean-in-jsx)
 
 [className 2](https://medium.com/@rohitdeshpande9922/why-react-uses-classname-rather-than-class-1d656317995d)
+
+[emojis vsCode](https://www.youtube.com/watch?v=AyVzFZu-Ox8&ab_channel=CodeCraftAcademy)
+
+[useState](https://react.dev/reference/react/useState)
+
+[useEffect](https://react.dev/reference/react/useEffect)
+
+[useEffect runs twice](https://react.dev/reference/react/useEffect#my-effect-runs-twice-when-the-component-mounts)
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
 
 const Card = ({ title }) => {
   const [hasLiked, setHasLiked] = useState(false);
+
+  // HOOK useEffect(), ci permette di eseguire del codice in determinati momenti, ad esempio all'avvio del component, al destroy del component o se cambia lo state
+  // questo hook è un metodo che ha la seguente sintassi: useEffect(callback con codice sa eseguire, [dependencies array])
+  // nel dependencies array indichiamo quando deve essere eseguito il codice, ad esempio se indichiamo nell'array [hasLinked] il codice verrà eseguito solo quando la variabile hasLinked cambierà di stato (verrà chiamato anche quando la variabile viene valorizzata nel componente con il valore di default)
+  // [] array vuoto vuol dire che la callback deve essere chiamata quando il componente è instanziato
+  // useEffect(() => console.log('like'), []);
+  // se non passo un array come secondo argomento la callback viene chiamata per ogni cambiamento del componente
+  useEffect(() => console.log('like'), [hasLiked]);
 
   return (
     <div className="card">
